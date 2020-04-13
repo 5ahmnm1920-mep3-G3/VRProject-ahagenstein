eba4f8709523b352b298686c392898440a0a6fc8
ansohag <anna.hagenstein@htl-salzburg.ac.at>
2020-04-13 15:00:53 +0200

    created EggCollector Script, imported new globe, created globe animation

---
+++ Assets/MyGame/Scripts/EggCollector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+
+
+public class EggCollector : MonoBehaviour
+{
+    private const string EGG_TAG = "egg";
+    private int eggCount;
+
+    private void OnTriggerEnter(Collider coll)
+    {
+        //check ob egg, wenn nicht dann geh einfach
+        if (!coll.CompareTag(EGG_TAG)) return;
+        //füge ein egg hinzu
+        eggCount++; print(eggCount);
+        //cheaten nicht ermöglichen, Eier nicht mehr aus Pot nehmen können 
+        coll.GetComponent<Interactable>().enabled = false;
+    }
+
+
+}
