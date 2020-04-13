9f60f47bba168bb1155ab437ba519eb46981fbec
ansohag <anna.hagenstein@htl-salzburg.ac.at>
2020-04-13 15:55:50 +0200

    globe animation starts after every egg and rabbit animaiton starts when you put all eggs in the pot, Audio starts when you put all eggs in the pot

--- Assets/MyGame/Scripts/EggCollector.cs
+++ Assets/MyGame/Scripts/EggCollector.cs
@@ -9,6 +9,9 @@ public class EggCollector : MonoBehaviour
 {
     private const string EGG_TAG = "egg";
     private int eggCount;
+    public Animation globeAnimation;
+    public Animation rabbitAnimation;
+    public AudioSource happyNoise;
 
     private void OnTriggerEnter(Collider coll)
     {
@@ -16,9 +19,20 @@ public class EggCollector : MonoBehaviour
         if (!coll.CompareTag(EGG_TAG)) return;
         //füge ein egg hinzu
         eggCount++; print(eggCount);
+        //Spiele Globus animation
+        globeAnimation.Play();
         //cheaten nicht ermöglichen, Eier nicht mehr aus Pot nehmen können 
         coll.GetComponent<Interactable>().enabled = false;
+        //wenn 5 Eier im Pot dann epic party
+        if (eggCount >= 5)
+        {
+            rabbitAnimation.Play();
+            happyNoise.Play();
+        }
+
     }
+
+
 
 
 }
